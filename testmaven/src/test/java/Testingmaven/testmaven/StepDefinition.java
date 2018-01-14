aaff08a8e725c1fb05dda6bdd1f170f22610972a
shivashankar2020 <shivashankar2020@gmail.com>
2018-01-14 21:36:18 +0000

    adding step

--- testmaven/src/test/java/Testingmaven/testmaven/StepDefinition.java
+++ testmaven/src/test/java/Testingmaven/testmaven/StepDefinition.java
@@ -10,6 +10,11 @@ import cucumber.api.java.en.Then;
 public class StepDefinition {
 
 	public WebDriver driver = Configuration.browser();
+	public ReadExcel read;
+
+	public StepDefinition() {
+		read = new ReadExcel();
+	}
 
 	@Given("^I want to navigate to dvla site \"([^\"]*)\"$")
 	public void i_want_to_navigate_to_dvm_site(String arg1) {
@@ -19,22 +24,22 @@ public class StepDefinition {
 	@Then("^click on next button and enter vehcile registration code and submit$")
 	public void click_on_next_button_and_enter_vehcile_registration_code_and_submit() throws Throwable {
 		driver.findElement(By.xpath("//a[@href='https://www.vehicleenquiry.service.gov.uk']")).click();
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("DY03 HYM");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys(read.readData("VehicleNumber"));
 		driver.findElement(By.name("Continue")).click();
 
 	}
 
 	@Then("^validate vechile details$")
 	public void validate_vechile_details() {
-		Assert.assertTrue("color is not displayed", driver.findElement(By.xpath("//strong[text()='BLACK']")).isDisplayed());
-		
+		Assert.assertTrue("color is not displayed",
+				driver.findElement(By.xpath("//strong[text()='BLACK']")).isDisplayed());
+
 	}
-	
+
 	@Then("^validate vechile details of color$")
 	public void validate_vechile_model() {
 		Assert.assertTrue("Model of the color", driver.findElement(By.xpath("//strong[text()='FORD']")).isDisplayed());
-		
-	}
 
+	}
 
 }
